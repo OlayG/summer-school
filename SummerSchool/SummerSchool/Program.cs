eec54c620e470ffac382d91b4a60fff03a9abd14
OlayG <ogureje@yahoo.com>
2016-06-17 14:40:50 -0400

    User Welcome and Selections

--- SummerSchool/SummerSchool/Program.cs
+++ SummerSchool/SummerSchool/Program.cs
@@ -8,26 +8,41 @@ namespace SummerSchool
 {
     class Program
     {
+        static string[] Students = new string[15];
+
         static void Main(string[] args)
         {
-            EnrollStudent();
-            UnEnrollStudent();
-            PrintStudentList();
+            //EnrollStudent();
+            //UnEnrollStudent();
+            //PrintStudentList();
+
+            Console.WriteLine("Welcome, input the number that corresponds to your choice selection");
+
+            Console.WriteLine();
+            Console.WriteLine("1. Enroll a student");
+            Console.WriteLine("2. Unenroll a student");
+            Console.WriteLine("3. Print out the list of enrolled students");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+
         }
 
-        private static void PrintStudentList()
-        {
-            throw new NotImplementedException();
-        }
+        //private static void PrintStudentList()
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        private static void UnEnrollStudent()
-        {
-            throw new NotImplementedException();
-        }
+        //private static void UnEnrollStudent()
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        private static void EnrollStudent()
-        {
-            throw new NotImplementedException();
-        }
+        //private static void EnrollStudent()
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
