498607ea21106255139fe84738e5c91277104039
OlayG <ogureje@yahoo.com>
2016-06-18 06:57:01 -0400

    Basic core of program working. No special cases yet

--- SummerSchool/SummerSchool/Program.cs
+++ SummerSchool/SummerSchool/Program.cs
@@ -9,40 +9,175 @@ namespace SummerSchool
     class Program
     {
         static string[] Students = new string[15];
+        static int[] StudentsAccountBalance = new int[15];
 
         static void Main(string[] args)
         {
-            //EnrollStudent();
-            //UnEnrollStudent();
-            //PrintStudentList();
-
-            Console.WriteLine("Welcome, input the number that corresponds to your choice selection");
-
-            Console.WriteLine();
-            Console.WriteLine("1. Enroll a student");
-            Console.WriteLine("2. Unenroll a student");
-            Console.WriteLine("3. Print out the list of enrolled students");
-            Console.WriteLine("4. Exit");
-            Console.WriteLine();
-
+            Menu();
+            Console.WriteLine("You are now Signed Out!");
 
             Console.ReadKey();
 
         }
 
-        //private static void PrintStudentList()
-        //{
-        //    throw new NotImplementedException();
-        //}
+        private static int CheckEnrollmentStatus()
+        {
+            int counter = 0;
+            for (int i = 0; i < Students.Length; i++)
+            {
+                if (Students[i] != null)
+                {
+                    counter++;
+                }
+            }
 
-        //private static void UnEnrollStudent()
-        //{
-        //    throw new NotImplementedException();
-        //}
+            return counter;
+        }
 
-        //private static void EnrollStudent()
-        //{
-        //    throw new NotImplementedException();
-        //}
+        private static void PrintStudentList()
+        {
+            for (int i = 0; i < Students.Length; i++)
+            {
+                if (Students[i] != null)
+                {
+                    Console.WriteLine();
+                    Console.Write( i+1 + ". " + Students[i] + " ");
+                    Console.Write("(£" + StudentsAccountBalance[i] + ")");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private static void UnEnrollStudent()
+        {
+            string[] tempArray = new string[15];
+            int[] tempArrayBal = new int[15];
+            PrintStudentList();
+            Console.WriteLine();
+            Console.WriteLine("Which student would you like to unenroll?");
+            int StudentToUnenroll = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            string StudentName = Students[StudentToUnenroll - 1];         
+
+            for (int i = 0, j = 0; i < 15; i++)
+            {
+
+                if (i == StudentToUnenroll - 1)
+                {
+                    Students[i] = null;
+                    StudentsAccountBalance[i] = 0;
+                }
+
+                if(Students[i] != null)
+                {
+                    tempArray[j] = Students[i];
+                    tempArrayBal[j] = StudentsAccountBalance[i];
+                    j++;
+                }
+            }
+
+            Students = tempArray;
+            StudentsAccountBalance = tempArrayBal;
+            Console.WriteLine(StudentName + " has been unenrolled.");
+            Console.WriteLine();
+        }
+
+        private static void EnrollStudent()
+        {
+            int EnrollmentCost = 200;
+            Console.WriteLine();
+            Console.WriteLine("Please enter student's name");
+            string StudentName = Console.ReadLine();
+            Console.WriteLine();
+
+            if (StudentName.ToLower() == "malfoy")
+            {
+                Console.WriteLine("Enrollment declined. The Malfoy family has been BANNED from our institution.");
+            } else if (StudentName.ToLower() == "potter")
+            {
+                for (int i = 0; i < 15; i++)
+                {
+                    if (Students[i] == null)
+                    {
+                        Students[i] = StudentName;
+                        StudentsAccountBalance[i] = EnrollmentCost/2;
+                        Console.WriteLine("Thank you for choosing us {0}.\nYour currently balance is an amount of: -{1:C}", StudentName, EnrollmentCost);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 15; i++)
+                {
+                    if (Students[i] == null)
+                    {
+                        Students[i] = StudentName;
+                        StudentsAccountBalance[i] = EnrollmentCost;
+                        Console.WriteLine("Thank you for choosing us {0}.\nYour currently balance is an amount of: -{1:C}", StudentName, EnrollmentCost);
+                        break;
+                    }
+                }
+            }
+           
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine("Welcome, input the number that corresponds to your choice selection");
+            int selection;
+            do
+            {
+                Console.WriteLine();
+                if (CheckEnrollmentStatus() == 15)
+                {
+                    Console.WriteLine("2. Unenroll a student");
+                    Console.WriteLine("3. Print out the list of enrolled students");
+                    Console.WriteLine("4. Exit");
+                    Console.WriteLine();
+                }
+                else if (CheckEnrollmentStatus() == 0)
+                {
+                    Console.WriteLine("1. Enroll a student");
+                    Console.WriteLine("3. Print out the list of enrolled students");
+                    Console.WriteLine("4. Exit");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("1. Enroll a student");
+                    Console.WriteLine("2. Unenroll a student");
+                    Console.WriteLine("3. Print out the list of enrolled students");
+                    Console.WriteLine("4. Exit");
+                    Console.WriteLine();
+                }
+
+
+
+                selection = Convert.ToInt32(Console.ReadLine());
+
+                if (selection == 1 && CheckEnrollmentStatus() != 15)
+                {
+                    EnrollStudent();
+                }
+                else if (selection == 2 && CheckEnrollmentStatus() != 0)
+                {
+                    UnEnrollStudent();
+
+                }
+                else if (selection == 3)
+                {
+                    PrintStudentList();
+                }
+                else
+                {
+                    continue;
+                }
+
+
+
+            } while (selection != 4);
+        }
     }
 }
